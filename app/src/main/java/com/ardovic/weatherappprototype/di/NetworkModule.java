ac85c8ecb0011f5e607987350cff789e9344ebde
Serj Ardovic <ardovic@gmail.com>
2018-05-15 00:14:10 +0300

    Added logginf interceptor (okhttp), updated gradle dependencie versions.

--- app/src/main/java/com/ardovic/weatherappprototype/di/NetworkModule.java
+++ app/src/main/java/com/ardovic/weatherappprototype/di/NetworkModule.java
@@ -6,6 +6,8 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -20,8 +22,15 @@ public class NetworkModule {
     @Provides
     @Singleton
     Retrofit provideRetrofit() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }

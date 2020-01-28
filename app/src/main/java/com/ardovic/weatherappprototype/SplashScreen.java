e1879ac6a8349ca39e38fa0eadf385e8af9f63d3
gouri-panda <gouripanda4@gmail.com>
2020-01-29 00:13:31 +0530

    Bug fixes
    more bug fixes

--- app/src/main/java/com/ardovic/weatherappprototype/SplashScreen.java
+++ app/src/main/java/com/ardovic/weatherappprototype/SplashScreen.java
@@ -37,6 +37,7 @@ public class SplashScreen extends BaseActivity {
         super.onCreate(savedInstanceState);
         IF_TABLE_EXITS = sharedPreferences.getBoolean(TABLE,false);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
         imageView = findViewById(R.id.splash_screen);
         imageView.setVisibility(View.INVISIBLE);
         getSupportActionBar().hide();
@@ -44,13 +45,17 @@ public class SplashScreen extends BaseActivity {
 
         if (IF_TABLE_EXITS){
             Log.d(TAG, "onCreate: data base exists");
-            startActivity(new Intent(SplashScreen.this,MainActivity.class));
+            Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }else {
             imageView.setVisibility(View.VISIBLE);
             Log.d(TAG, "onCreate: data base doesn't exits");
             createLocalCityDB();
             sharedPreferences.edit().putBoolean(TABLE, true).apply();
-            startActivity(new Intent(SplashScreen.this,MainActivity.class));
+            Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
 
 

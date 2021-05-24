88ebb8024a3adb36460b425c4b967a7a9bb8d9a6
aditya3901 <2020itb058.aditya@students.iiests.ac.in>
2021-05-24 09:50:23 +0530

    Implemented Google-SignIn

--- app/src/main/java/com/ardovic/weatherappprototype/activities/MainActivity.java
+++ app/src/main/java/com/ardovic/weatherappprototype/activities/MainActivity.java
@@ -45,10 +45,24 @@ import com.ardovic.weatherappprototype.R;
 import com.ardovic.weatherappprototype.database.DatabaseHelper;
 import com.ardovic.weatherappprototype.fragments.CreditFragment;
 import com.ardovic.weatherappprototype.model.IJ;
+import com.ardovic.weatherappprototype.model.Users;
 import com.ardovic.weatherappprototype.model.retrofit.Response;
 import com.ardovic.weatherappprototype.util.ImageHelper;
 import com.ardovic.weatherappprototype.services.LocationService;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
@@ -113,6 +127,10 @@ public class MainActivity extends BaseActivity implements LoaderManager.LoaderCa
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     public boolean manualSearchFlag = false;
 
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth mAuth;
+    FirebaseDatabase mFirebaseDatabase;
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -138,7 +156,16 @@ public class MainActivity extends BaseActivity implements LoaderManager.LoaderCa
             setContentView(R.layout.activity_main);
             ButterKnife.bind(this);
 
-            // Initializations
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Initializations
             drawerLayout = findViewById(R.id.drawer_layout);
             navigationView = findViewById(R.id.nav_view);
             toolbar = findViewById(R.id.toolbar_main);
@@ -221,8 +248,72 @@ public class MainActivity extends BaseActivity implements LoaderManager.LoaderCa
 
             // Activate navigation drawer's listener
             activateDrawerListener();
-
     }
+
+
+    int RC_SIGN_IN = 65;
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                assert account != null;
+                Log.d("TAG", "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("TAG", "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            assert user != null;
+                            String name = user.getDisplayName();
+                            TextView username = findViewById(R.id.username);
+                            username.setText(name);
+                            Toast.makeText(MainActivity.this, "Welcome " + name, Toast.LENGTH_SHORT).show();
+
+                            //Storing User's Data to Firebase Database
+                            Users users = new Users();
+                            users.setUserID(user.getUid());
+                            users.setUserName(user.getDisplayName());
+                            users.setProfilePic(Objects.requireNonNull(user.getPhotoUrl()).toString());
+                            users.setMail(user.getEmail());
+
+                            //Uploading user data to Firebase Realtime DB
+                            mFirebaseDatabase.getReference().child("Users").child(user.getUid()).setValue(users);
+
+                            //Now you can easily implement the Profile Page of the app by using the data store in Users Model
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                        }
+                    }
+                });
+    }
+
+
 
     /// check if device is a tablet or a phone ///
     public void isTablet(Context context) {
@@ -390,7 +481,7 @@ public class MainActivity extends BaseActivity implements LoaderManager.LoaderCa
                 toast_not_implemented_yet.show();
                 return true;
             case R.id.nav_auth: // Handle the camera action
-                toast_not_implemented_yet.show();
+                signIn();
                 return true;
             case R.id.nav_forecastType:
                 toast_not_implemented_yet.show();
@@ -412,7 +503,10 @@ public class MainActivity extends BaseActivity implements LoaderManager.LoaderCa
                 toast_not_implemented_yet.show();
                 return true;
             case R.id.action_logout:
-                toast_not_implemented_yet.show();
+                mAuth.signOut();
+                TextView username = findViewById(R.id.username);
+                username.setText("<Username>");
+                Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
                 return true;
             default:
                 toast_not_implemented_yet.show();
